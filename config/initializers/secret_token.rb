be3b02060cf92622f2bbc32e5756385d326cf2ff
kenn <kennmunene@gmail.com>
2013-04-26 23:19:27 +0300

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Kennapp::Application.config.secret_token = 'ad534163a887b6c3aec5d4243b4164ac814d7632d05510ad0db48aa1102844f984949c8c793ce5fdad54be1fd3929ea63f90f07aa69ec1a5b76fa8a3e3d57e0b'
